c97ef0bc2d8197ffe157d65a38c5d2291baa034e
GeorgeMagdy <georgmagdy2@yahoo.com>
2019-04-12 13:47:56 -0700

    Todo list complete with datasource and deleg

---
+++ Todoey/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by George on 4/12/19.
+//  Copyright © 2019 GeorgeMagdy. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+}
